b5e5efd5ad90a8e00720207e078ff4fcd63e889f
Alex <alex_mcgarry@hotmail.co.uk>
2026-04-27 19:36:14 +0100

    add data, collections, and helpers

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,54 @@
+// Filters
+import { dateFilter } from "./src/filters/date-filter.js";
+import { w3DateFilter } from "./src/filters/w3-date-filter.js";
+import rssPlugin from "@11ty/eleventy-plugin-rss";
+
 export default function (eleventyConfig) {
-  eleventyConfig.setInputDirectory('src');
-  eleventyConfig.setOutputDirectory('dist');
-  eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.setInputDirectory("src");
+  eleventyConfig.setOutputDirectory("dist");
+  // Add filters
+  eleventyConfig.addFilter("dateFilter", dateFilter);
+  eleventyConfig.addFilter("w3DateFilter", w3DateFilter);
+  // Set directories to pass through to the dist folder
+  eleventyConfig.addPassthroughCopy("src/images");
+  // Plugins
+  eleventyConfig.addPlugin(rssPlugin);
+
+  // Returns work items, sorted by display order
+  eleventyConfig.addCollection("work", (collection) => {
+    return sortByDisplayOrder(collection.getFilteredByGlob("./src/work/*.md"));
+  });
+  // Creates and returns a collection of work that is set to be featured
+  eleventyConfig.addCollection("featuredWork", (collection) => {
+    return sortByDisplayOrder(
+      collection.getFilteredByGlob("./src/work/*.md"),
+    ).filter((x) => x.data.featured);
+  });
+  // Returns a collection of blog posts in reverse date order
+  eleventyConfig.addCollection("blog", (collection) => {
+    return [...collection.getFilteredByGlob("./src/posts/*.md")].reverse();
+  });
+  // Returns a list of people ordered by filename
+  eleventyConfig.addCollection("people", (collection) => {
+    return collection.getFilteredByGlob("./src/people/*.md").sort((a, b) => {
+      return Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1;
+    });
+  });
 }
 
 export const config = {
-  markdownTemplateEngine: 'njk',
-  htmlTemplateEngine: 'njk',
+  markdownTemplateEngine: "njk",
+  htmlTemplateEngine: "njk",
 };
+
+/**
+ * Takes a collection and returns it back in display order
+ *
+ * @param {Array} collection The 11ty collection
+ * @returns {Array} the sorted collection
+ */
+function sortByDisplayOrder(collection) {
+  return collection.sort((a, b) =>
+    Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1,
+  );
+}
--- src/index.md
+++ src/index.md
@@ -1,14 +1,26 @@
 ---
-title: 'Hello, world'
-layout: 'layouts/home.html'
+title: "Issue 33"
+metaDesc: "A made up agency site that you build if you follow the Learn Eleventy course"
+layout: "layouts/home.html"
 intro:
-  eyebrow: 'Digital Marketing is our'
-  main: 'Bread & Butter'
-  summary: 'Let us help you create the perfect campaign with our multi-faceted team of talented creatives.'
-  buttonText: 'See our work'
-  buttonUrl: '/work'
-  image: '/images/bg/toast.jpg'
-  imageAlt: 'Buttered toasted white bread'
----
+  eyebrow: "Digital Marketing is our"
+  main: "Bread & Butter"
+  summary: "Let us help you create the perfect campaign with our multi-faceted team of talented creatives."
+  buttonText: "See our work"
+  buttonUrl: "/work"
+  image: "/images/bg/toast.jpg"
+  imageAlt: "Buttered toasted white bread"
 
-This is pretty _rad_, right?
+primaryCTA:
+  title: "This is an agency that doesn't actually exist"
+  summary: 'This is the project site you build when you take the "Learn Eleventy" course so it is all made up as a pretend context. You will learn a lot about Eleventy by building this site though. Take the course today!'
+  buttonText: "It's free!"
+  buttonUrl: "https://learneleventy.dev"
+
+featuredWork:
+  title: "Selected work"
+  summary: "Some stuff that should give you an idea of what we’re all about."
+
+studioFeed:
+  title: "From inside the studio"
+---
